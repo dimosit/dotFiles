95f60a976a6f8adc9828820dd1156232f6bb4cb8
dimosit <dimos.it21149@gmail.com>
2022-01-31 01:24:33 +0200

    apt install zsh -y

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
+echo Installing ZSH 
+apt install zsh -y
+
 # Find all dot files then if the original file exists, create a backup
 # Once backed up to {file}.dtbak symlink the new dotfile in place
 for file in $(find . -maxdepth 2 -name ".*" -type f  -printf "%f\n" ); do
